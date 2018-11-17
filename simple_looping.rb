2f144a74ede60455140515e5509cae04db0e2677
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-17 00:49:16 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -19,7 +19,6 @@ end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times.do
     phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase 
